7ea0b37dd877d1518428b3ca09452c0651f014e8
yuuyauchi <yuuyauchi1998@gmail.com>
2023-09-08 10:50:50 +0000

    333

--- src/utils/aaa.py
+++ src/utils/aaa.py
@@ -1,7 +1,3 @@
-import wikipedia
-
-wikipedia.set_lang("jp")
-import pdb;pdb.set_trace()
-word="リオネルメッシ"
-words = wikipedia.search(word)
-wikipedia.summary(words[0])
+import tinysegmenter
+segmenter = tinysegmenter.TinySegmenter()
+print(' | '.join(segmenter.tokenize("2010年、オフィシャル・ニュートリション・パーソナル・スポンサーとしてHerbalife社と契約し、Herbalife社の恵まれない子供たちへの支援活動「Casa HERBALIFE」に協力。Herbalife社の製品を愛用していることを自身のブログサイトで述べている。")))
