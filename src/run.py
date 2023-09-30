fd0655f6530cbda43a15793873c37a35121d76ca
yuuyauchi <yuuyauchi1998@gmail.com>
2023-09-30 20:19:57 +0900

    add chatgpt model

--- src/run.py
+++ src/run.py
@@ -1,12 +1,15 @@
 import argparse
 
+import openai
 import streamlit as st
 from models.langchain_model import LangChainChatBot
 from models.llamaindex_model import LlamaindexChatBot
 
 parser = argparse.ArgumentParser(description="使用するモデルを記述")
 parser.add_argument("model_name")
+parser.add_argument("model_id")
 args = parser.parse_args()
+model_id = None
 
 if args.model_name == "llamaindex":
     query_engine = LlamaindexChatBot.deploy()
@@ -15,6 +18,8 @@ elif args.model_name == "langchain":
     chatbot.read_data()
     chatbot.preprocess()
     query_engine = chatbot.generate_engine()
+elif args.model_name == "chatgpt":
+    model_id = args.model_id
 else:
     raise NameError("モデル名の引数が間違っている或いは入力されていません。")
 
@@ -31,7 +36,13 @@ if prompt := st.chat_input("質問を入力してください。"):
     st.session_state.messages.append({"role": "user", "content": prompt})
 
     with st.spinner("回答を生成中..."):
-        response = query_engine.query(prompt)
+        if args.model_name in ["llamaindex", "langchain"]:
+            response = query_engine.query(prompt)
+        elif args.model_name == "chatgpt":
+            response = openai.ChatCompletion.create(
+                model=model_id, messages=[{"role": "user", "content": prompt}]
+            )
+            response = response.to_dict_recursive()["choices"][0]["message"]["content"]
 
     with st.chat_message("assistant"):
         st.markdown(response)
